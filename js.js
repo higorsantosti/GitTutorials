2706cfa687d3c061e3e19101f2beba0decd501cc
Higor Santos <higorsantos.ti@gmail.com>
2023-09-12 16:38:59 -0300

    curso youtube 4h JS

--- js.js
+++ js.js
@@ -4,23 +4,47 @@ console.log(idade);
 let altura = 180;
 console.log(altura);
 
-
 const pika = "teste"
 console.log(pika);
-
 
 //criando objeto {}
 
 let pessoa = {
-    nome: "higor",
-    idade: 29,
-    sexo: "M"
+    nome: "higor",    idade: 29,    sexo: "M"
 };
 
 console.log(pessoa);
 
 //criando Array []
 
-let familia = [26,'higor',20,true];
+let familia = [26,'higor',true];
 
 console.log(familia[1]);
+
+console.log(familia.length);
+
+// FUNCTION ()
+let corSite = "azul"
+
+function resetaCor(){ 
+    corSite = "branco";
+};
+
+console.log(corSite);
+//chamando function
+resetaCor();
+console.log(corSite);
+///////////////////////
+function resetaCor(cor, tonalidade){ 
+    corSite = cor + ' ' + tonalidade ;
+};
+
+
+console.log(corSite);
+resetaCor("amarelo", "claro");
+console.log(corSite);
+
+
+//    https://www.youtube.com/watch?v=i6Oi-YtXnAU&t=393s
+
+//  aos 38 minutos
