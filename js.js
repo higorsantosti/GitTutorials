4d59cdf2efbac1c0118bf51727e74bf0c6990c10
Higor Santos <higorsantos.ti@gmail.com>
2023-09-12 15:41:02 -0300

    curso youtube 4h JS

---
+++ js.js
@@ -0,0 +1,20 @@
+let idade = 29;
+console.log(idade);
+
+let altura = 180;
+console.log(altura);
+
+
+const pika = "chupa meu pau"
+console.log(pika);
+
+
+//criando objeto
+
+let pessoa = {
+    nome: "higor",
+    idade: 29,
+    sexo: "M"
+};
+
+console.log(pessoa);
