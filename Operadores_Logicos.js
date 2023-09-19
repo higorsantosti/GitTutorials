23d4dc241380f2d8b4c2cd0efb499db5c8395c49
Higor Santos <higorsantos.ti@gmail.com>
2023-09-19 08:50:56 -0300

    Estudos

---
+++ Operadores_Logicos.js
@@ -0,0 +1,24 @@
+
+
+
+
+// Operador Logico e (&&) ou ||
+
+
+
+
+let maiorDeIdade = false;
+
+let possuiCarteiraTrabalho = false;
+
+let podeAplicar = maiorDeIdade || possuiCarteiraTrabalho;
+
+console.log('pode aplicar: ', podeAplicar);
+
+
+//Operador NOT (!)
+
+let candidatoRecusado = !podeAplicar;
+
+console.log('candidato Recusado', candidatoRecusado);
+
