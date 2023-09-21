c71e1ce59b495b43295ff2ca45d87d7f80c505cb
Higor Santos <higorsantos.ti@gmail.com>
2023-09-21 16:52:35 -0300

    exercicio

---
+++ Exercicios.js
@@ -0,0 +1,28 @@
+
+
+
+// trocar valro de variavel
+
+// exercicios
+
+
+let a = 'vermelho';
+let b = 'azul';
+
+let c = a;
+a = b;
+b = c;
+
+
+console.log(a);
+console.log(b);
+
+
+// if..Else
+// se a hora estiver entre 6 e 12 é bom dia
+// se for entre 12 e 18 é boa tarde
+// se contrario é boa noite
+
+// https://www.youtube.com/watch?v=i6Oi-YtXnAU&t=393s
+
+// 1:11h
